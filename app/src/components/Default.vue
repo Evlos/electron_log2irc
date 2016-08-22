4c2dfe90cb068dc75312b7eff0e46ca4a1b39b38
Evlos <4tyle8@gmail.com>
2016-08-22 16:53:48 +0800

    [+] handlers and display of status

--- app/src/components/Default.vue
+++ app/src/components/Default.vue
@@ -1,6 +1,10 @@
-<style lang="sass" scoped>
+<style lang="sass">
 *
   font-family: Lato, "Helvetica Neue", Arial, Helvetica, sans-serif
+body
+  width: 100%
+  height: 100%
+  overflow: hidden
 img
   margin-top: -25px
   width: 450px
@@ -35,11 +39,11 @@ span.status
   color: white
 .buttonGroup
   font-size: 14px
+  display: inline-block
   > div
     display: inline-block
   > :last-child
-    position: relative
-    left: -20px
+    margin-left: -20px
   div:last-child
     background: #E0E1E2
     padding: 10px 21px
@@ -92,13 +96,15 @@ span.status
 
 <template lang="pug">
 .box
-  .buttonGroup
-    button.button(v-on:click='onClickFileSelector') Select a file
-    .middle
-      span or
-    .last
-      span paste file url
-      input.input(v-model='path')
+  p
+    .buttonGroup
+      button.button(v-on:click='onClickFileSelector') Select a file
+      .middle
+        span or
+      .last
+        span paste file url
+        input.input(v-model='path')
+    span.status {{ status.path }}.
   p
     .inputGroup
       span IRC Nickname
@@ -110,120 +116,173 @@ span.status
   p
     button.button.green.marginRight(v-on:click='onClickConnectToIRC') Connect to IRC
     button.button(v-on:click='onClickSaySometing') Say "I'm a bot!"
-    span.status {{ ircStatus }}.
-  .buttonGroup
-    button.button.green(v-on:click='onClickStartWatch') Start watch
-    .middle
-      span or
-    button.button(v-on:click='onClickStopWatch') Stop watch
+    span.status {{ status.irc }}.
   p
-    textarea.textarea(v-bind:value="_.slice(data, data.length - 3).reverse().join('\n')")
+    .buttonGroup
+      button.button.green(v-on:click='onClickStartWatch') Start watch
+      .middle
+        span or
+      button.button(v-on:click='onClickStopWatch') Stop watch
+    span.status {{ status.watching }}.
+  p
+    .small latest log:
+    textarea.textarea(v-bind:value="_.slice(logs, logs.length - 3).reverse().join('\n')")
+  p
+    .small IRC:
+    textarea.textarea(v-bind:value="_.slice(chat, chat.length - 3).reverse().join('\n')")
 </template>
 
 <script>
-  import CurrentPage from './Default/CurrentPage'
-  import Versions from './Default/Versions'
-  var ipc = require('electron').ipcRenderer
-  var Tail = require('tail').Tail
-  var irc = require('irc')
+import CurrentPage from './Default/CurrentPage'
+import Versions from './Default/Versions'
+var ipc = require('electron').ipcRenderer
+var Tail = require('tail').Tail
+var irc = require('irc')
+var fs = require('fs')
 
-  export default {
-    components: {
-      CurrentPage,
-      Versions
-    },
-    name: 'default',
-    data () {
-      return {
-        ircClient: null,
-        ircStatus: 'disconnected',
-        nick: 'bot___',
-        channel: '#test',
-        path: '/var/log/system.log',
-        tail: null,
-        data: [],
-      }
-    },
-    watch: {
-      'nick': {
-        handler (val, old) {
-          localStorage.setItem('nick', val)
-        },
+export default {
+  components: {
+    CurrentPage,
+    Versions
+  },
+  name: 'default',
+  data () {
+    return {
+      client: null,
+      status: {
+        irc: 'disconnected',
+        path: 'file not found',
+        watching: 'not watching',
       },
-      'path': {
-        handler (val, old) {
-          localStorage.setItem('path', val)
-        },
-      },
-      'channel': {
-        handler (val, old) {
-          localStorage.setItem('channel', val)
-        },
-      },
-      'data': {
-        handler (val, old) {
-          if (val.length > 100) {
-            this.data = []
-          }
-        },
-      },
-    },
-    methods: {
-      onClickFileSelector() {
-        ipc.send('open-file-dialog')
-      },
-      onClickConnectToIRC() {
-        this.ircStatus = 'connecting ..'
-        this.ircClient = new irc.Client('chat.freenode.net', this.nick, {
-          showErrors: true,
-          autoRejoin: true,
-          channels: [this.channel],
-          retryCount: 3,
-          retryDelay: 3000,
-        })
-        this.ircClient.addListener('message', (from, to, message) => {
-          console.debug('IRC |', from + ' => ' + to + ': ' + message);
-        });
-        this.ircClient.addListener('names', (from, to, message) => {
-          this.ircStatus = 'connected'
-        });
-      },
-      onClickSaySometing() {
-        this.ircClient.say(this.channel, "I'm a bot!");
-      },
-      onClickStartWatch() {
-        this.tail = new Tail('/var/log/system.log')
-        this.tail.on('line', (data) => {
-          // console.debug(data)
-          this.data.push(data)
-          if (this.ircClient) {
-            this.ircClient.say(this.channel, data);
-          }
-        })
-        this.tail.on('error', (error) => {
-          console.log('error:', error)
-        })
-      },
-      onClickStopWatch() {
-        this.tail.unwatch()
-      },
-    },
-    created() {
-      ipc.on('selected-directory', (event, path) => {
-        this.path = path
-      })
-      let nick = localStorage.getItem('nick')
-      if (nick) {
-        this.nick = nick
-      }
-      let path = localStorage.getItem('path')
-      if (path) {
-        this.path = path
-      }
-      let channel = localStorage.getItem('channel')
-      if (channel) {
-        this.channel = channel
-      }
+      nick: 'bot___',
+      channel: '#test',
+      path: '/var/log/system.log',
+      tail: null,
+      logs: [],
+      chat: [],
     }
-  }
+  },
+  watch: {
+    'nick': {
+      handler (val, old) {
+        localStorage.setItem('nick', val)
+      },
+    },
+    'path': {
+      handler (val, old) {
+        localStorage.setItem('path', val)
+        this.checkPath()
+      },
+    },
+    'channel': {
+      handler (val, old) {
+        localStorage.setItem('channel', val)
+      },
+    },
+    'logs': {
+      handler (val, old) {
+        if (val.length > 100) {
+          this.logs = _.slice(logs, logs.length - 3)
+        }
+      },
+    },
+    'chat': {
+      handler (val, old) {
+        if (val.length > 100) {
+          this.chat = _.slice(chat, chat.length - 3)
+        }
+      },
+    },
+  },
+  methods: {
+    onClickFileSelector() {
+      ipc.send('open-file-dialog')
+    },
+    onClickConnectToIRC() {
+      this.status.irc = 'connecting ..'
+      this.client = new irc.Client('chat.freenode.net', this.nick, {
+        showErrors: true,
+        autoRejoin: true,
+        channels: [this.channel],
+        retryCount: 3,
+        retryDelay: 3000,
+      })
+      this.client.addListener('motd', (motd) => {
+        this.chat.push('channel | motd: ' + motd)
+      });
+      this.client.addListener('topic', (channel, topic, nick, message) => {
+        this.chat.push('channel | topic: ' + topic + ', ' + message)
+      });
+      this.client.addListener('join', (channel, nick, message) => {
+        this.chat.push('channel | user: ' + nick + ' joined')
+      });
+      this.client.addListener('quit', (nick, reason, channels, message) => {
+        this.chat.push('channel | user: ' + nick + ' quitted')
+      });
+      this.client.addListener('message', (sender, channel, message) => {
+        this.chat.push(sender + ': ' + message)
+      });
+      this.client.addListener('selfMessage', (to, text) => {
+        this.chat.push(this.nick + ': ' + text)
+      });
+      this.client.addListener('names', (sender, channel, message) => {
+        this.status.irc = 'connected'
+      });
+      this.client.addListener('error', (message) => {
+        this.chat.push('error: ' + message)
+      });
+      this.client.addListener('ping', (message) => {
+        this.chat.push('ping: ' + message)
+      });
+    },
+    onClickSaySometing() {
+      if (this.client) this.client.say(this.channel, 'I\'m a bot!');
+    },
+    onClickStartWatch() {
+      this.status.watching = 'watching ..'
+      this.tail = new Tail('/var/log/system.log')
+      this.tail.on('line', (logs) => {
+        this.logs.push(logs)
+        if (this.client) {
+          this.client.say(this.channel, logs)
+        }
+      })
+      this.tail.on('error', (error) => {
+        this.logs.push('error: ' + error)
+      })
+    },
+    onClickStopWatch() {
+      this.tail.unwatch()
+      this.status.watching = 'not watching'
+    },
+    checkPath() {
+      fs.exists(this.path, (exists) => {
+        if (exists) this.status.path = 'file located';
+        else this.status.path = 'file not found';
+      });
+    },
+  },
+  created() {
+    ipc.on('selected-directory', (event, path) => {
+      this.path = path
+    })
+    let nick = localStorage.getItem('nick')
+    if (nick) {
+      this.nick = nick
+    }
+    let path = localStorage.getItem('path')
+    if (path) {
+      this.path = path
+    }
+    let channel = localStorage.getItem('channel')
+    if (channel) {
+      this.channel = channel
+    }
+    this.checkPath()
+    window.onerror = function (e, url, lineNumber, column, errorObj) {
+      this.chat.push('onerror: ' + errorMsg)
+      e.preventDefault ? e.preventDefault() : null
+    }
+  },
+}
 </script>
